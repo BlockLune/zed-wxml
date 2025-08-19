31885b5cc80455f2e83ea255aafc7e065c7528f4
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-08-19 17:31:39 +0800

    feat: simplify outline

--- languages/wxml/outline.scm
+++ languages/wxml/outline.scm
@@ -2,46 +2,30 @@
 
 (element
   (start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @context))) @item
+    (tag_name) @name)) @item
 
 (element
   (self_closing_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @context))) @item
+    (tag_name) @name)) @item
 
 (block_element
   (block_start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @context))) @item
+    (tag_name) @name)) @item
 
 (slot_element
   (slot_start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @context))) @item
+    (tag_name) @name)) @item
 
 (template_element
   (template_start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @context))) @item
+    (tag_name) @name)) @item
 
 (wxs_element
   (wxs_start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @context))) @item
+    (tag_name) @name)) @item
 
 (import_statement
-  (tag_name) @name
-  (attribute
-    (attribute_name) @context)) @item
+  (tag_name) @name) @item
 
 (include_statement
-  (tag_name) @name
-  (attribute
-    (attribute_name) @context)) @item
+  (tag_name) @name) @item
